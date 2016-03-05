685d1693756aa02256781adc2b1f6d3bb3ec8c80
Aidan Musnitzky <musnit@gmail.com>
2016-03-05 15:51:53 +0700

    Remove misc

--- restApi/lib/index.js
+++ restApi/lib/index.js
@@ -4,17 +4,6 @@ var dynamo = new doc.DynamoDB();
 // Single - All
 module.exports.singleAll = function(event, cb) {
 
-  var response = {
-    message: 'Your Serverless function ran successfully via the \''
-    + event.httpMethod
-    + '\' methoq!'
-    + ' event.resourcePath: '
-    + event.resourcePath
-    + ' event.modelName: '
-    + event.modelName
-    + ' event.pathId: '
-    + event.pathId
-  };
   var operation = event.httpMethod;
   var dynamoCallback = function(error, data){
     cb(error, data);
