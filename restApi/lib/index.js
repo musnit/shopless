6fdbb2a1d3459ae0522571e31e6a7339fe0105d7
Aidan Musnitzky <musnit@gmail.com>
2016-03-21 16:27:46 -0700

    Add and get vogels working with scan method, and extract filter params properly

--- restApi/lib/index.js
+++ restApi/lib/index.js
@@ -1,8 +1,35 @@
 var doc = require('dynamodb-doc');
 var dynamo = new doc.DynamoDB();
 
+var AWS = require('aws-sdk');
+var vogels = require('vogels');
+var Joi = require('joi');
+
+AWS.config.update({region : 'us-east-1'});
+var dynamodb = new AWS.DynamoDB();
+vogels.dynamoDriver(dynamodb);
+
+var extractFilters = function(queryParamsString) {
+  var queryParamsArray = queryParamsString.slice(1, queryParamsString.length).split(',');
+  queryParamsArray = queryParamsArray.map(function(queryParam){
+    return queryParam.trim().split('=');
+  });
+  queryParamsArray = queryParamsArray.filter(function(queryParam){
+    return queryParam[0].indexOf('filter[') === 0;
+  });
+  var filterParams = {};
+  queryParamsArray.forEach(function(queryParam){
+    var key = queryParam[0].slice(7, queryParam[0].length - 1);
+    filterParams[key] = queryParam[1];
+  });
+  return filterParams;
+}
+
 // Single - All
 module.exports.singleAll = function(event, cb) {
+
+  var filterParams = extractFilters(event.queryParams);
+  var hasFilters = Object.keys(filterParams).length !== 0;
 
   var operation = event.httpMethod;
   var dynamoCallback = function(error, data){
@@ -29,8 +56,14 @@ module.exports.singleAll = function(event, cb) {
       }.bind(this));
       break;
     case 'GET':
+      var Model = vogels.define(event.modelName, {
+        hashKey : 'email',
+        // add the timestamp attributes (updatedAt, createdAt)
+        timestamps : true,
+        tableName: event.modelName
+      });
       if(typeof event.pathId === undefined || event.pathId === ""){
-        dynamo.scan(params, dynamoCallback);
+        Model.scan().exec(dynamoCallback);
       }
       else {
         params.Key = { name: event.pathId };
