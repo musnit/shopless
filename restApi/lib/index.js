39815c1e240264127bc914f3b0df6eb6142c3390
Aidan Musnitzky <musnit@gmail.com>
2016-06-04 21:37:30 -0700

    Use vogels more and improve include support

--- restApi/lib/index.js
+++ restApi/lib/index.js
@@ -43,7 +43,16 @@ module.exports.singleAll = function(event, cb) {
   };
 
   var tableCallback = function(error, data){
-    dataContextHolder = data;
+    if(data["Items"]) {
+      dataContextHolder = {
+        Items: data["Items"]
+      };
+    }
+    else {
+      dataContextHolder = {
+        Items: [data]
+      };
+    }
     if (include){
       var Model = vogels.define(include, {
         hashKey : 'id',
@@ -53,61 +62,46 @@ module.exports.singleAll = function(event, cb) {
         }
       });
 
-      if (data["Items"]){
-        var includeIds = data["Items"].map(function(item){
-          return item.attrs[include];
-        });
-
-        var query = Model.scan();
-        query.where('name').in(includeIds);
-        query.exec(includeCallback);
-      }
-      else {
-        var params = {
-          TableName: include,
-          Key: { name: data["Item"][include] }
-        };
-        dynamo.getItem(params, includeCallback);
-      }
+      var includeIds = dataContextHolder.Items.map(function(item){
+        return item.attrs[include];
+      });
+      Model.getItems(includeIds, includeCallback);
     }
     else {
-      dynamoCallback(error, data);
+      dynamoCallback(error, dataContextHolder);
     }
   };
 
   var includeCallback = function(error, data){
+    var includeContextHolder;
+    if(data["Items"]) {
+      includeContextHolder = {
+        Items: data["Items"]
+      };
+    }
+    else {
+      includeContextHolder = {
+        Items: [data]
+      };
+    }
     dataContextHolder.include = {};
-    dataContextHolder.include[include] = data;
+    dataContextHolder.include[include] = includeContextHolder;
     dynamoCallback(error, dataContextHolder);
   };
 
-  var params = {
-    TableName: event.modelName
-  };
+  var Model = vogels.define(event.modelName, {
+    hashKey : 'id',
+    tableName: event.modelName,
+    schema : {
+      id : vogels.types.uuid()
+    }
+  });
 
   switch (operation) {
     case 'POST':
-      params.Item = event.body;
-      dynamo.putItem(params, function(error, data){
-        if (error) {
-          cb(error, undefined);
-        }
-        else {
-          dynamo.getItem({
-            TableName: event.modelName,
-            Key: { name: event.body.name }
-          }, dynamoCallback);
-        }
-      }.bind(this));
+      Model.create(event.body, dynamoCallback);
       break;
     case 'GET':
-      var Model = vogels.define(event.modelName, {
-        hashKey : 'id',
-        tableName: event.modelName,
-        schema : {
-          id : vogels.types.uuid()
-        }
-      });
       if(typeof event.pathId === undefined || event.pathId === ""){
         var query = Model.scan();
         for(var key in filterParams) {
@@ -118,33 +112,15 @@ module.exports.singleAll = function(event, cb) {
         query.exec(tableCallback);
       }
       else {
-        params.Key = { name: event.pathId };
-        dynamo.getItem(params, tableCallback);
+        Model.get(event.pathId, tableCallback);
       }
       break;
     case 'PATCH':
-      params.Key = { name: event.pathId };
-      var mappedBody = {};
-      Object.keys(event.body).map(function(value, index) {
-       mappedBody[value] = { Value: event.body[value] };
-      });
-      params.AttributeUpdates = mappedBody;
-      dynamo.updateItem(params, function(error, data){
-        if (error) {
-          cb(error, undefined);
-        }
-        else {
-          dynamo.getItem({
-            TableName: event.modelName,
-            Key: { name: event.pathId }
-          }, dynamoCallback);
-        }
-      }.bind(this));
-
+      event.body.id = event.pathId;
+      Model.update(event.body, dynamoCallback);
       break;
     case 'DELETE':
-      params.Key = { name: event.pathId };
-      dynamo.deleteItem(params, dynamoCallback);
+      Model.destroy(event.pathId, dynamoCallback);
       break;
     default:
       return cb(new Error('Unrecognized operation "' + operation + '"'));
